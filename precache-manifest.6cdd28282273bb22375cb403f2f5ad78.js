8bd984198f23be9225407bd9ec2222abc2abab74
Abhishek K <abhishekakade@gmail.com>
2019-02-04 01:40:10 +0530

    Updates

---
+++ precache-manifest.6cdd28282273bb22375cb403f2f5ad78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "617e3c50e67378d4af10",
+    "url": "/robo-friends/static/js/runtime~main.617e3c50.js"
+  },
+  {
+    "revision": "dd921a5fafec08ced979",
+    "url": "/robo-friends/static/js/main.dd921a5f.chunk.js"
+  },
+  {
+    "revision": "a08cb7cee6b8e89fbbdb",
+    "url": "/robo-friends/static/js/1.a08cb7ce.chunk.js"
+  },
+  {
+    "revision": "dd921a5fafec08ced979",
+    "url": "/robo-friends/static/css/main.42f4a70e.chunk.css"
+  },
+  {
+    "revision": "a08cb7cee6b8e89fbbdb",
+    "url": "/robo-friends/static/css/1.27326db2.chunk.css"
+  },
+  {
+    "revision": "40ca455eacddf968951ba6c74221629f",
+    "url": "/robo-friends/index.html"
+  }
+];
