a789a5686ca302936d06263aa7fe1ab3e6e2519f
Abhishek K <abhishekakade@gmail.com>
2019-02-03 23:04:54 +0530

    Initial commit with GH-Pages

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import CardList from './CardList';
+import { robots } from './robots';
+import SearchBox from './SearchBox';
+
+const App = () => {
+
+  return (
+    <div className="tc">
+      <h1 className="tc">RoboFriends</h1>
+      <SearchBox />
+      <CardList robots={robots} />
+    </div>
+  );
+
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-import CardList from './CardList';
 import 'tachyons';
-import { robots } from './robots';
-
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  
-  <CardList robots={robots} />, 
-  document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
