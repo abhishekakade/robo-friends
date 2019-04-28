e6e28046b1a5c65cb73d468c960e7732798f2461
Abhishek K <abhishekakade@gmail.com>
2019-04-28 23:56:06 +0530

    Updates

---
+++ precache-manifest.6c2b88c0e765d71de0cf2037583a4b82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo-friends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "e26fa2088cf320dc503e",
+    "url": "/robo-friends/static/js/runtime~main.04d6e5ad.js"
+  },
+  {
+    "revision": "fc83bb64bb6954dcc24d",
+    "url": "/robo-friends/static/js/main.11fbf1ca.chunk.js"
+  },
+  {
+    "revision": "1d0c0a238ff0815c4a6b",
+    "url": "/robo-friends/static/js/2.ee5d9378.chunk.js"
+  },
+  {
+    "revision": "fc83bb64bb6954dcc24d",
+    "url": "/robo-friends/static/css/main.20abb96f.chunk.css"
+  },
+  {
+    "revision": "1d0c0a238ff0815c4a6b",
+    "url": "/robo-friends/static/css/2.07b64970.chunk.css"
+  },
+  {
+    "revision": "e11e4b44a61f9f65a4a8afff426ed921",
+    "url": "/robo-friends/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/robo-friends/precache-manifest.dc4eac8936791a56fb6de42c590724c1.js"
+  "/robo-friends/precache-manifest.6c2b88c0e765d71de0cf2037583a4b82.js"
 );
 
 workbox.clientsClaim();
